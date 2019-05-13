f7b7cbf87ac9f02aab06de4bf7a2a255154b3564
Isa Giongo <isagiongo@gmail.com>
2019-05-13 20:48:42 -0300

    Adiciona testes de integracao

--- src/test/java/com/isagiongo/pragmabrewery/integration/PilsnerVerifierIntegrationTest.java
+++ src/test/java/com/isagiongo/pragmabrewery/integration/PilsnerVerifierIntegrationTest.java
@@ -71,4 +71,16 @@ public class PilsnerVerifierIntegrationTest {
 				.body("message", Matchers.equalTo("Inappropriate Temperature"))
 				.statusCode(HttpStatus.EXPECTATION_FAILED.value());
 	}
+	
+	@Test
+	public void shouldValidateIfBeerNameIsValid() {
+		RestAssured
+				.given()
+				.contentType(ContentType.JSON)
+				.body("{\"temperature\": -5}")
+				.post("/v1/beers/{beerName}/validations", "ISA")
+				.then()
+				.body("message", Matchers.equalTo("Invalid Beer"))
+				.statusCode(HttpStatus.BAD_REQUEST.value());
+	}
 }
